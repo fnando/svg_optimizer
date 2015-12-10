53936f7af670692459df38b62491b4c4d67a079b
Nando Vieira <fnando.vieira@gmail.com>
2015-12-10 12:43:17 -0200

    Remove deprecation.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,9 +20,7 @@ module RSpecHelpers
   end
 end
 
-
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = "random"
