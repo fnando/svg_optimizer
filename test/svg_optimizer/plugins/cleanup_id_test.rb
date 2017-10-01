59fd8978232763a4bfe3ce2bbdb23cad1c43a75c
Nando Vieira <fnando.vieira@gmail.com>
2017-09-30 23:30:07 -0700

    Cleanup IDs even when xlink namespace is not available. Close #6.

--- test/svg_optimizer/plugins/cleanup_id_test.rb
+++ test/svg_optimizer/plugins/cleanup_id_test.rb
@@ -2,16 +2,27 @@
 
 require "test_helper"
 
-class CleanupIdTest < Minitest::Test
-  plugin_class SvgOptimizer::Plugins::CleanupId
-  with_svg_plugin "cleanup_id.svg"
+module CleanupIdTest
+  class CleanupIdWithXLinkNamespaceTest < Minitest::Test
+    plugin_class SvgOptimizer::Plugins::CleanupId
+    with_svg_plugin "cleanup_id.svg"
 
-  test "applies plugin" do
-    assert_equal "a", xml.css("linearGradient").first["id"]
-    assert_equal "url(#a)", xml.css("circle").first["fill"]
-    assert xml.css("circle[id]").empty?
-    assert xml.css("g[id]").empty?
-    assert_equal 2, xml.css("tref[xlink|href='#b']").size
-    assert_equal "b", xml.css("text").first["id"]
+    test "applies plugin" do
+      assert_equal "a", xml.css("linearGradient").first["id"]
+      assert_equal "url(#a)", xml.css("circle").first["fill"]
+      assert xml.css("circle[id]").empty?
+      assert xml.css("g[id]").empty?
+      assert_equal 2, xml.css("tref[xlink|href='#b']").size
+      assert_equal "b", xml.css("text").first["id"]
+    end
+  end
+
+  class CleanupIdWithoutXLinkNamespaceTest < Minitest::Test
+    plugin_class SvgOptimizer::Plugins::CleanupId
+    with_svg_plugin "cleanup_id_without_xlink.svg"
+
+    test "applies plugin" do
+      assert_equal File.read("./test/fixtures/cleanup_id_without_xlink_expected.svg").to_s.chomp, xml.root.to_s
+    end
   end
 end
